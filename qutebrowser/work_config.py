14bd81c3dd6a882ca8d1701f04158f77eadb5beb
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2023-07-21 10:57:33 -0300

    Adjust IP for work browser's proxy

--- qutebrowser/work_config.py
+++ qutebrowser/work_config.py
@@ -3,5 +3,5 @@ from base_config import load_base_config
 
 config.load_autoconfig()
 load_base_config(c, config)
-c.content.proxy = "http://192.168.0.119:8899"
+c.content.proxy = "http://192.168.0.120:8899"
 c.content.notifications.enabled = True
