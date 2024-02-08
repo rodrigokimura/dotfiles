0c069927f26d93609726f5369a3e52001deba730
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2024-02-08 10:55:48 -0300

    Extract proxy IP as config

--- qutebrowser/work_config.py
+++ qutebrowser/work_config.py
@@ -1,7 +1,9 @@
 from base_config import load_base_config
+import os
 
+work_proxy_ip = os.getenv("WORK_PROXY_IP")
 
 config.load_autoconfig()
 load_base_config(c, config)
-c.content.proxy = "http://192.168.0.120:8899"
+c.content.proxy = f"http://{work_proxy_ip}:8899"
 c.content.notifications.enabled = True
